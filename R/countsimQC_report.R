7bc9b25ee7c1d0caf80b839e5b1fb65acfd19ee0
Charlotte Soneson <charlottesoneson@gmail.com>
2017-05-16 11:16:14 +0200

    Modified figure sizes. Allow output figures to be saved.

--- R/countsimQC_report.R
+++ R/countsimQC_report.R
@@ -18,9 +18,15 @@
 #'   \code{output_dir} directory.
 #'
 countsimQC_report <- function(dds_list, show_code = FALSE, output_format = NULL,
-                            output_file = NULL, output_dir = "./", ...){
-  ## This function was inspired by code from Nicholas Hamilton, obtained from
+                              output_file = NULL, output_dir = "./", ...){
+  ## This function was inspired by code from Nicholas Hamilton, provided at
   ## http://stackoverflow.com/questions/37097535/generate-report-in-r
+
+  if (file.exists(paste0(output_dir, "/", gsub(".html", "_figures", basename(output_file)))))
+    stop("The folder ",
+         paste0(output_dir, "/", gsub(".html", "_figures", basename(output_file))),
+         ", where the figures will be generated, already exists. ",
+         "Please remove or rename the folder, or provide another value of output_file.")
 
   ## Path to the template file
   template_file <- system.file("extdata", "simulation_comparison_template_list.Rmd",
@@ -29,11 +35,16 @@ countsimQC_report <- function(dds_list, show_code = FALSE, output_format = NULL,
   ## Process the arguments
   args <- list(...)
   args$input <- template_file
-  args$output_dir <- output_dir
   args$output_format <- output_format
   args$output_file <- output_file
 
   ## Render the report
   output_file <- do.call("render", args = args)
+
+  ## Move output files
+  file.rename(output_file, paste0(output_dir, "/", basename(output_file)))
+  file.rename(gsub(".html", "_figures", output_file),
+              paste0(output_dir, "/", gsub(".html", "_figures", basename(output_file))))
+
   invisible(output_file)
 }
