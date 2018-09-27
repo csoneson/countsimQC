6a8d72f7319915be6dcfbe15b32cf0cf189ca265
Charlotte Soneson <charlottesoneson@gmail.com>
2018-09-27 11:57:44 +0200

    Add example and documentation to correlation calculations

--- R/calculateCorrelations.R
+++ R/calculateCorrelations.R
@@ -1,17 +1,31 @@
 #' Calculate Spearman correlation between sample pairs
 #'
+#' This function is typically not called directly by the user, but is used when
+#' generating the countsimQC report.
+#'
 #' @param ddsList List of lists, with one element per data set. Each element is
 #'   a list containing a DGEList and a DESeqDataSet, with calculated
-#'   dispersions.
-#' @param maxNForCorr If the number of samples in a data set exceeds
+#'   dispersions (e.g., output from \code{\link{calculateDispersionsddsList}}).
+#' @param maxNForCorr Maximal number of samples to use for correlation
+#'   calculation. If the number of samples in a data set exceeds
 #'   \code{maxNForCorr}, \code{maxNForCorr} samples will be randomly selected
-#'   for correlation calculation.
-#' @param seed The seed for downsampling
+#'   for calculation of correlations.
+#' @param seed The random seed used for downsampling/selection of samples.
 #'
 #' @return A data frame with pairwise sample correlations for each data set
 #' @author Charlotte Soneson
 #' @importFrom stats cor
 #' @importFrom edgeR cpm
+#'
+#' @examples
+#' data(countsimExample)
+#' ## Subset genes to speed up calculations
+#' sub <- lapply(countsimExample, function(x) x[1:100, ])
+#' ## Calculate dispersions
+#' obj <- calculateDispersionsddsList(ddsList = sub,
+#'                                    maxNForDisp = 5, seed = 123)
+#' ## Calculate sample correlations
+#' corrs <- calculateSampleCorrs(obj, maxNForCorr = 5, seed = 123)
 #'
 calculateSampleCorrs <- function(ddsList, maxNForCorr, seed = 123) {
   sampleCorrDF <- lapply(ddsList, function(x) {
@@ -32,19 +46,33 @@ calculateSampleCorrs <- function(ddsList, maxNForCorr, seed = 123) {
 
 #' Calculate Spearman correlation between feature pairs
 #'
+#' This function is typically not called directly by the user, but is used when
+#' generating the countsimQC report.
+#'
 #' @param ddsList List of lists, with one element per data set. Each element is
 #'   a list containing a DGEList and a DESeqDataSet, with calculated
-#'   dispersions.
-#' @param maxNForCorr If the number of features in a data set exceeds
+#'   dispersions (e.g., output from \code{\link{calculateDispersionsddsList}}).
+#' @param maxNForCorr Maximal number of features to use for calculation of
+#'   correlations. If the number of features in a data set exceeds
 #'   \code{maxNForCorr}, \code{maxNForCorr} features will be randomly selected
-#'   for correlation calculation.
-#' @param seed The seed for downsampling
+#'   for calculation of correlations.
+#' @param seed The random seed used for downsampling/selection of features.
 #'
 #' @return A data frame with pairwise feature correlations for each data set
 #' @author Charlotte Soneson
 #' @importFrom stats cor
 #' @importFrom edgeR cpm
 #' @importFrom genefilter rowVars
+#'
+#' @examples
+#' data(countsimExample)
+#' ## Subset genes to speed up calculations
+#' sub <- lapply(countsimExample, function(x) x[1:100, ])
+#' ## Calculate dispersions
+#' obj <- calculateDispersionsddsList(ddsList = sub,
+#'                                    maxNForDisp = 5, seed = 123)
+#' ## Calculate feature correlations
+#' corrs <- calculateFeatureCorrs(obj, maxNForCorr = 5, seed = 123)
 #'
 calculateFeatureCorrs <- function(ddsList, maxNForCorr, seed = 123) {
   featureCorrDF <- lapply(ddsList, function(x) {
