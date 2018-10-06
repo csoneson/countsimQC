580267d4e9e2cedfce693be034eeda3b47bbf230
Charlotte Soneson <charlottesoneson@gmail.com>
2018-10-06 10:29:22 +0200

    Extend documentation of example data sets.

--- R/countsimExample.R
+++ R/countsimExample.R
@@ -3,6 +3,14 @@
 #' A named list with three elements, each corresponding to a (real or simulated)
 #' count data set.
 #'
+#' The \code{Original} data set represents a subset of 10,000 genes and 11 cells
+#' from the GSE74596 single-cell RNA-seq data set, obtained from the conquer
+#' repository (http://imlspenticton.uzh.ch:3838/conquer/). The \code{Sim1} and
+#' \code{Sim2} data sets similarly represent subsets of scRNA-seq data sets
+#' simulated with two different simulation methods, using the real GSE74596 data
+#' set as the basis for parameter estimation. Each data set is represented as a
+#' \code{DESeqDataSet} object.
+#'
 #' @format A named list with three elements, each corresponding to a (real or
 #'   simulated) count data set.
 #'
--- R/countsimExample_dfmat.R
+++ R/countsimExample_dfmat.R
@@ -4,6 +4,13 @@
 #' count data set. One of them is provided as a DESeqDataset, one as a count
 #' data frame and one as a count matrix.
 #'
+#' The \code{Original} data set represents a subset of 10,000 genes and 11 cells
+#' from the GSE74596 single-cell RNA-seq data set, obtained from the conquer
+#' repository (http://imlspenticton.uzh.ch:3838/conquer/). The \code{Sim1} and
+#' \code{Sim2} data sets similarly represent subsets of scRNA-seq data sets
+#' simulated with two different simulation methods, using the real GSE74596 data
+#' set as the basis for parameter estimation.
+
 #' @format A named list with three elements, each corresponding to a (real or
 #'   simulated) count data set.
 #'
