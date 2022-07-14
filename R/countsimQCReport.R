515265c916e676ca5af2ed6419827c4f70b0cf2f
Charlotte Soneson <charlottesoneson@gmail.com>
2022-07-14 10:58:20 +0200

    Add argument to activate ragg

--- R/countsimQCReport.R
+++ R/countsimQCReport.R
@@ -125,6 +125,8 @@
 #'   ignorePandoc is TRUE, only a warning is given. The figures will be
 #'   generated, but not the final report. If ignorePandoc is FALSE (default),
 #'   the execution stops immediately.
+#' @param useRAGG Logical scalar, indicating whether to use ragg_png as the
+#'   graphics device in the report rather than the default png.
 #' @param ... Other arguments that will be passed to \code{rmarkdown::render}.
 #'
 #' @author Charlotte Soneson
@@ -166,7 +168,7 @@ countsimQCReport <- function(ddsList, outputFile, outputDir = "./",
                              kfrac = 0.01, kmin = 5,
                              permutationPvalues = FALSE, nPermutations = NULL,
                              knitrProgress = FALSE, quiet = FALSE,
-                             ignorePandoc = FALSE, ...){
+                             ignorePandoc = FALSE, useRAGG = FALSE, ...){
   ## This function was inspired by code from Nicholas Hamilton, provided at
   ## http://stackoverflow.com/questions/37097535/generate-report-in-r
 
